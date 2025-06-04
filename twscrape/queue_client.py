435a40aeb29e0ffb3287e6cb4dd8585de8c99ff5
water_pluss <water_plus@outlook.com>
2025-06-04 13:07:30 +0800

    set active if twitter account rate limit exceede

--- twscrape/queue_client.py
+++ twscrape/queue_client.py
@@ -198,7 +198,7 @@ class QueueClient:
         # no way to check is account banned in direct way, but this check should work
         if err_msg.startswith("(88) Rate limit exceeded") and limit_remaining > 0:
             logger.warning(f"Ban detected: {log_msg}")
-            await self._close_ctx(-1, inactive=True, msg=err_msg)
+            await self._close_ctx(limit_reset)
             raise HandledError()
 
         if err_msg.startswith("(326) Authorization: Denied by access control"):

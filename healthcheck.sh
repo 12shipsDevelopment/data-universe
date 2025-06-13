90531120156aad72c8428f2a5bf400b8908bfc8f
Xiechengqi <Xiechengqi01@gmail.com>
2025-06-13 14:27:51 +0800

    Update healthcheck.sh
    Signed-off-by: Xiechengqi <Xiechengqi01@gmail.com>

--- healthcheck.sh
+++ healthcheck.sh
@@ -9,8 +9,8 @@
 main() {
 
 local total=$(twscrape stats | grep -oP 'Total: [0-9]+' | awk '{print $NF}')
-local active=$(twscrape stats | grep 'locked_SearchTimeline' | awk '{print $NF}')
-[ ".${active}" = "." ] && local active=$(twscrape stats | grep -oP 'Active: [0-9]+' | awk '{print $NF}')
+local active=$(twscrape stats | grep 'locked_SearchTimeline' | awk '{print $NF}' | sed 's/,//g')
+[ ".${active}" = "." ] && local active=$(twscrape stats | grep -oP 'Active: [0-9]+' | awk '{print $NF}' | sed 's/,//g')
 local activePercent=$(echo "(100 * ${active}) / ${total}" | bc)
 [ "${activePercent}" -lt "20" ] && echo "Current active accounts number ${active}, total accounts number ${total}, not healthy, restarting ..." && exit 1
 echo "Current active accounts number ${active}, total accounts number ${total}, healthy"
